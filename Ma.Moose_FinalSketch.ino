bbfc7818329e54e798adaa41208136792c24cb6f
pgoodjohn <bongiovanni.pietro.vc@gmail.comc>
2016-03-28 11:24:15 +0800

    Edited some comments and reordered includes

--- Ma.Moose_FinalSketch.ino
+++ Ma.Moose_FinalSketch.ino
@@ -7,7 +7,7 @@
 */
 
 /*
-Notes on the sd should be named as follow in order for this baseline code to run properly
+Notes on the sd should be named as follow in order for this code to run properly
 TRACK 000 = DO
 TRACK 001 = DO#
 TRACK 002 = RE
@@ -23,19 +23,17 @@ TRACK 011 = SI
 */
 
 #include <MPR121.h>
-#include <Wire.h>
-#include <SPI.h>
 #include <SdFat.h>
 #include <SdFatUtil.h>
 #include <SFEMP3Shield.h>
+#include <SPI.h>
+#include <Wire.h>
 
 #define MPR121_ADDR 0x5C
 #define MPR121_INT 4
 #define SIZE 11
 
-//playtrack is the variable in which is stored the value of the track being played, it gets updated every time
-//a new pin is touched, when the touchboard is fired up there is no value stored in this variable
-//it gets a value as soon as one pin (or one finger of the glove is pressed)
+//Playtrack represents which note is being played by the board.
 int playtrack;
 
 //I tried making it work with some arrays but the TouchBoard wasn't too happy about it and refused to work
@@ -52,55 +50,52 @@ int track8;
 int track9;
 int track10;
 int track11;
-//declaration for the two potentiometers, the actual values are defined some lines below, mapped and stuff
+//Declarations for the two potentiometers.
 int pot1 = 0;
 int pot2 = 0;
 
-//initializing sd reader and mp3 player
+//Initialization of SD Reader and MP3 Player
 SdFat sd;
 SFEMP3Shield player;
 
 void setup() {
   Serial.begin(9600);
 
-  //checking if mp3 player is working
+  //Check for the touch listener
   if (!MPR121.begin(MPR121_ADDR)) Serial.println("error setting up MPR121");
   MPR121.setInterruptPin(MPR121_INT);
 
-  //checking if sd is working correctly
+  //Check for the SD Reader
   if (!sd.begin(SD_SEL, SPI_HALF_SPEED)) {
     sd.initErrorHalt();
   }
 
-  //initializing mp3 player
+  //Initialization of the MP3 Player
   player.begin();
   player.setVolume(0, 0);
 
-  //threshold of touch
+  //Setting the threshold of the capacitive buttons
   MPR121.setTouchThreshold(8);
   MPR121.setReleaseThreshold(5);
 
-  //defining the inputs for the potentiometers, standard stuff for this arduino stuff
+  //Defining the two potentiometers in pin Input0 and Input1
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 }
 
 void loop() {
-  //read and map potentiometer for scales
+  //Reading the values of the potentiometers and mapping them accordingly
   pot1 = map(analogRead(A0), 0, 1023, 0, 11);
   pot2 = map(analogRead(A1), 0, 1023, 1, 4);
 
   Serial.println(pot1, pot2);
 
-  //Pot1 defines the first note of the tracks so that pot2 can build the scales with algorithms
-  //pot1 is mapped above to return values from 0 to 11 which correspond to the notes saved on the micro sd
-  //card, by deciding the first note of the scale with a potentiometer we can use a second one to build on this
-  //one scales by simply adding values to select other samples, which is done by the switch based on pot2
+  //Pot1 is used to define the first note of the scale
+  //Pot2 is used to decide which scale to build on top of the note chosen by Pot1
   track0 = pot1;
 
-  //switch for pot 2, deciding the type of scale which is built on the first note defined by pot 1
   if (pot2 == 1) {
-    //major armonic
+    //Major Armonic Scale
     track1 = track0 + 2;
     track2 = track0 + 4;
     track3 = track0 + 5;
@@ -115,7 +110,7 @@ void loop() {
     Serial.println("Maj");
   }
   if (pot2 == 2) {
-    //minor armonic
+    //Minor Armonic Scale
     track1 = track0 + 1;
     track2 = track0 + 2;
     track3 = track0 + 4;
@@ -130,7 +125,7 @@ void loop() {
     Serial.println("min");
   }
   if (pot2 == 3) {
-    //major pentatonic (1, 2, 3, 5, 6tones)
+    //Major Penthatonic Scale
     track1 = track0 + 2;
     track2 = track0 + 3;
     track3 = track0 + 5;
@@ -145,7 +140,7 @@ void loop() {
     Serial.println("Penthatonic Maj");
   }
   if (pot2 == 4) {
-    //minor pentatonic (1, 3, 4, 5, 7 tones)
+    //Minor Penthatonic Scale
     track1 = track0 + 2;
     track2 = track0 + 3;
     track3 = track0 + 4;
@@ -160,19 +155,15 @@ void loop() {
     Serial.println("Penthatonic Min");
   }
 
-  //this serie of function of MPR121 allows us to operate with the capacitive pins on the touchboard
-  //the first condition triggers when any of the pin is touched and triggers the whole cycle
+  //Touch listener controls
   if (MPR121.touchStatusChanged())
   {
-    //this second function returns the number of the pin touched so that the function can go to the correct
-    //part of the cycle and reproduce the corresponding sound
     MPR121.updateTouchData();
     if (MPR121.isNewTouch(0))
     {
-      //all these nested ifs are needed to check if two pins are pressed at the same moment...
-      //i still need to think about this, there is probably a better way to implement all this code.
       if (MPR121.isNewTouch(1))
       {
+        //Updating the playtrack variable with the new note to be played
         playtrack = track5;
       }
       else if (MPR121.isNewTouch(2))
@@ -187,13 +178,11 @@ void loop() {
       {
         playtrack = track0;
       }
-      //this line of code stops the sample that was previously playing so that a new sample can start as soon
-      //as i click one of the buttons on my glove
+      //Stopping whatever notes was playing
       player.stopTrack();
-      //this line triggers the sample which starts playing immediatly
+      //Retriggering MP3 Player with new note
       player.playTrack(playtrack);
-      //this line is just for debugging, prints to the serial number the number of track which is being
-      //played
+      //Printing which note is being played on the console
       Serial.println(playtrack);
     }
     if (MPR121.isNewTouch(1))
